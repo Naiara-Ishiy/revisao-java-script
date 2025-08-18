10eb38fc819659519d755f590330d57ecf7eb0e7
Naiara "Kim <naiaraishiyacademico@gmail.com>
2025-08-18 19:35:04 -0300

    Initial commit

---
+++ 002-variaveis.js
@@ -0,0 +1,24 @@
+//para rodar o programa, escreva node e o nome do documento
+let texto = 'Hoje não está muitop frio';
+let numero = 10;
+let numero1 = 12.7;
+var tituloTexto = 'Como tá o dia?';
+const escola = 'Etec Tupã';
+console.log('Novo valor');
+texto = 'Hoje está frio';
+console.log('texto');
+
+//concatenar texto
+let nome = 'Naiara';
+let sobrenome = ' Ishiy';
+
+console.log('');
+console.log('Exemplo concatenar');
+console.log(nome+sobrenome);
+console.log(nome,sobrenome);
+
+//undefined
+let salario;
+console.log(salario);
+salario = null;
+console.log(salario);
---
+++ 003-tipos.js
@@ -0,0 +1,41 @@
+//texto/string
+let cidade = 'Tupã';
+
+//inteiro/number
+let populacao = 62000;
+
+//decimal/number
+let temperaturaMedia = 34.5;
+
+//sim/nao/boolean
+let temMCDonalds = true;
+
+//undefined
+let totalAnimaisRua;
+
+//null
+let mortesPorTerremoto = null;
+
+//date
+const fundacaoCidade = new Date(1929,10,12);
+
+//objeto - tem erro na data
+let enderecoPrefeitura =
+{   logradouro : 'Praça da Bandeira',
+    numero: 1,
+    complemento: null,
+    cep: '17600000',
+    fundacaoCidade: `${fundacaoCidade.getDate()}/${fundacaoCidade.getMonth()}/${fundacaoCidade.getFullYear()}`};
+
+//array
+let principaisAvenidas = ['Tamoios','Aimorés','Lélio Pizza'];
+
+//função
+function ExibeDados()
+{    console.log (`
+        A cidade de ${cidade} tem ${populacao} habitantes.
+        A prefeitura fica localizada na ${enderecoPrefeitura.logradouro} nº ${enderecoPrefeitura.numero} - ${enderecoPrefeitura.cep}.
+        As principais avenidas são: ${principaisAvenidas[0]}, ${principaisAvenidas[1]} e ${principaisAvenidas[2]}.
+        Data de fundação: ${enderecoPrefeitura.fundacaoCidade}
+`);}
+ExibeDados();
