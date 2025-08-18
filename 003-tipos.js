0c1d7e5b32c716f5feb3f86f674466a06cedd6b9
Naiara "Kim <naiaraishiyacademico@gmail.com>
2025-08-18 20:32:53 -0300

    Revert "Instalação prompt-sync"
    This reverts commit 8842ddf7f9d1e875b41102cc67a4db9d838fee9d.

--- 003-tipos.js
+++ 003-tipos.js
@@ -39,8 +39,3 @@ function ExibeDados()
         Data de fundação: ${enderecoPrefeitura.fundacaoCidade}
 `);}
 ExibeDados();
-
-console.log('');
-console.log('Tipo da variável: ExibeDados');
-console.log(typeof(ExibeDados));
-console.log(Array.isArray(ExibeDados)); //diz se é o tipo de dado ou n
