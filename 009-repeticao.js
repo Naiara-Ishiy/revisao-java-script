8b7199e05ed87e25251cc279180028083b9dafec
Naiara "Kim <naiaraishiyacademico@gmail.com>
2025-08-25 21:39:25 -0300

    Aula- 25/08

---
+++ 009-repeticao.js
@@ -0,0 +1,10 @@
+let nInicio = 10;
+let nFim = 20;
+let repeticoes = 0;
+
+while(nInicio<=nFim){
+    console.log(nInicio);
+    nInicio++;
+    repeticoes++;}
+
+console.log(`O código foi repetido ${repeticoes} vezea!`)
